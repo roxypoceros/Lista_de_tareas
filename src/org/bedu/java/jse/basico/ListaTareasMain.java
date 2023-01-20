bec3eaca06e555368bad2742248f3e8f8b6f2394
roxypoceros <roxypoceros@gmail.com>
2023-01-20 01:57:03 -0600

    Primer commit con menu de opciones para proyecto

---
+++ src/org/bedu/java/jse/basico/ListaTareasMain.java
@@ -0,0 +1,14 @@
+package org.bedu.java.jse.basico;
+
+public class ListaTareasMain {
+    public static void main(String[] args) {
+        System.out.println("1. Crear nueva lista de tareas");
+        System.out.println("2. Ver listas de tareass");
+        System.out.println("3. Ver tareas de listas");
+        System.out.println("4. Actualizar lista de tareas");
+        System.out.println("5. Crear nueva lista de tareas");
+        System.out.println("6. Eliminar lista de tareas");
+
+
+    }
+}
