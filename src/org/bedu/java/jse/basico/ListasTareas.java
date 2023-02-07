83e40debb2b4cc0287df58afc66bc26c84916db4
roxypoceros <roxypoceros@gmail.com>
2023-02-06 19:09:10 -0600

    Sesion 6. Manejar objetos propios dentro de listas. Actualizar y eliminar elementos dentro de lista. Practicar el framework Collections.

--- src/org/bedu/java/jse/basico/ListasTareas.java
+++ src/org/bedu/java/jse/basico/ListasTareas.java
@@ -1,5 +1,8 @@
 package org.bedu.java.jse.basico;
 import org.bedu.java.jse.basico.modelo.ListaTareas;
+
+import java.util.ArrayList;
+import java.util.List;
 //Crear clase ListasTareas y
 // un método para cada una de las opciones del menú, dentro de cada método
 // x ahora solo es necesario mostrar un texto que indique el nombre de esa opción
@@ -7,6 +10,10 @@ import org.bedu.java.jse.basico.modelo.ListaTareas;
 public class ListasTareas {
 
     private Lector lector = new Lector();
+    //Crear nueva List para agrupar ListaTareas
+    //(las cuales a su vez contendrán Tareas).
+    //Declarar e inicializar una nueva instancia de esta forma
+    private List<ListaTareas> listasTareas = new ArrayList<>();
     public void crearNuevaLista(){
         System.out.println("Crear nueva lista de tareas.");
     //Usa el nuevo método leeCadena de Lector
@@ -15,31 +22,103 @@ public class ListasTareas {
     // pasando el nombre anterior en su constructor
     String nombre = lector.leeCadena();
     ListaTareas lista = new ListaTareas(nombre);
+    //Tomar ese objeto (nueva instancia de ListaTareas) y
+        // agrégalo a listasTareas:
+        listasTareas.add(lista);
     }
 
     public void verListaTareas(){
         System.out.println("Ver listas de tareas.");
+
+
+    //Si existe al menos una lista de tareas,
+    //muestra todos los elementos contenidos en listasTareas,
+    //indicando su índice y su nombre
+
+    for (int i = 0; i < listasTareas.size(); i++) {
+            System.out.println((i + 1) + " - " + listasTareas.get(i).getNombre());
+        }
     }
 
     public void verTareasDeLista() {
         System.out.println("Ver tareas de lista.");
         //usar el método leeOpcion para leer un valor numérico
-        byte lista = lector.leeOpcion();
+        byte indice = validaIndice();
+
+        if(indice == 0){
+            return;
+        }
+        ListaTareas lista = listasTareas.get(indice - 1);
+
+        if (lista.numeroTareas() == 0) {
+            System.out.println("Aún no hay tareas en la lista.");
+        }
+
+        lista.muestraTareas();
+
     }
 
     public void actualizarListaDeTareas(){
         System.out.println("Actualizar lista de tareas.");
         //usar el método leeOpcion para leer un valor numérico
 
-        byte lista = lector.leeOpcion();
+        byte indice = validaIndice();
+
+        if (indice == 0) {
+            return;
+        }
     }
 
     public void eliminarListaDeTareas(){
         System.out.println("Eliminar lista de tareas.");
+
+        byte indice = validaIndice();
+
+        if(indice == 0){
+            return;
+        }
+
+        ListaTareas listaEliminada = listasTareas.remove((indice - 1));
+
+        System.out.println("Se eliminó la lista de tareas: " + listaEliminada.getNombre());
     }
     //usar el método leeOpcion para leer un valor numérico
     byte lista = lector.leeOpcion();
 
+    //agregar método de utilidad que valide
+    //si ya existe alguna lista de tareas. /
+    //validación que estarás haciendo de forma constante,
+    //es una buena práctica agregarla dentro de un método
+    //que posteriormente puedas invocar.
+    //Valida si listasTareas ha sido inicializado y si no está vacía
+    private boolean validaExistenciaLista() {
+        if (listasTareas == null || listasTareas.isEmpty()) {
+            System.out.println("Aún no se ha creado ninguna lista de tareas.");
+            return false;
+        }
+        return true;
+    }
 
+    private byte validaIndice() {
+        byte indice = 0;
+        //Verificar que se tenga al menos una lista de tareas,
+        //usando el método que acabas de crear (validaExistenciaLista);
+        //si no hay ninguna lista de tareas entonces no hay nada que mostrar,
+        //por lo tanto, terminaremos la ejecución el método:
+
+        if (!validaExistenciaLista()) {
+            return indice;
+        }
+
+        System.out.println("Indique el índice de la lista de tareas.");
+        indice = lector.leeOpcion();
+
+        if (indice > listasTareas.size() || indice < 0) {
+            System.out.println("No existen listas de tareas en el índice seleccionado.");
+            indice = 0;
+        }
+
+        return indice;
+    }
 
 }
--- src/org/bedu/java/jse/basico/modelo/ListaTareas.java
+++ src/org/bedu/java/jse/basico/modelo/ListaTareas.java
@@ -1,10 +1,16 @@
 package org.bedu.java.jse.basico.modelo;
 import java.time.LocalDate;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ListaTareas {
     //Atributos
     private String nombre;
     private final LocalDate fechaCreacion;
+    //Para agrupar un conjunto de tareas relacionadas
+    private final List<Tarea> tareas = new ArrayList<>();
+
+
 
     //getters y setters
     public String getNombre() {
@@ -31,8 +37,37 @@ public class ListaTareas {
     {
         fechaCreacion = LocalDate.now();
     }
+    //Método que permite agregar una tarea
+    public void agregaTarea(Tarea tarea){
+        tareas.add(tarea);
+    }
+    //Método que permite eliminar una tarea
 
+    public Tarea eliminaTarea(int indice){
+        if(indice > tareas.size()){
+            System.out.println("La tarea indicada no se encuentra en la lista.");
+            return null;
+        }
 
+        return tareas.remove(indice);
+    }
+    //Método que permite obtener una lista de tareas
+
+    public List<Tarea> getTareas() {
+        return tareas;
+    }
+    //Método que permite obtener numero de tareas de lista
+
+    public int numeroTareas(){
+        return tareas.size();
+    }
+    //Método que imprime nombre de tareas
+
+    public void muestraTareas(){
+        for (int i = 0; i < tareas.size(); i++) {
+            System.out.println((i+1) + " - " + tareas.get(i).getNombre());
+        }
+    }
 
 
 
